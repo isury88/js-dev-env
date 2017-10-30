72694d62eb45a22cf2d8e3f46bf5f5e3e9de1f9f
Riccardo Venturini <riccardoventurini88@hotmail.com>
2017-10-30 06:24:47 +0000

    adding Mock Api Generate Fake Data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {getUsers} from './api/userApi';
+import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
 //Populate table of users via API call.
 getUsers().then(result => {
@@ -15,4 +16,17 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
-});
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //Must Use Array
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+      };
+    });
+  });
